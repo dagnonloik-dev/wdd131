e7c601939455e4bedbc9b409727074e6e8c4ec4a
Dagnon Loik <dagnonloik5@gmail.com>
2026-05-23 20:10:37 +0100

    First commit

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+let currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
+
+
